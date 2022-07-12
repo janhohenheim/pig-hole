f1e386511d9e17badfda5f8ccfc765bdb534c866
Jan Nils Ferner <contact@jnferner.com>
2022-07-12 13:40:57 +0200

    Implement main menu

--- src/menu/main_menu.rs
+++ src/menu/main_menu.rs
@@ -15,11 +15,17 @@ impl Plugin for MainMenuPlugin {
 fn show_menu(mut egui_ctx: ResMut<EguiContext>, mut state: ResMut<State<GameState>>) {
     egui::CentralPanel::default().show(egui_ctx.ctx_mut(), |ui| {
         ui.vertical_centered(|ui| {
-            ui.add_space(300.0);
+            ui.add_space(200.0);
             ui.heading("Pig Hole");
-            if ui.button("Play").clicked() {
-                state.set(GameState::JoiningLobby).unwrap()
-            }
+            ui.add_space(100.0);
+            let layout = egui::Layout::centered_and_justified(ui.layout().main_dir());
+            ui.allocate_ui_with_layout(egui::Vec2::new(300.0, 0.0), layout, |ui| {
+                ui.add_enabled(false, egui::Button::new("Quick Play"));
+                ui.add_enabled(false, egui::Button::new("Browse Games"));
+                if ui.button("Host Game").clicked() {
+                    state.set(GameState::JoiningLobby).unwrap()
+                }
+            });
         });
     });
 }
